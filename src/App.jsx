ac642dbe96cb56d0f476d9729b5019f534d20ca6
Kabz Jose <josekabz00@gmail.com>
2025-11-30 01:45:01 +0300

    Added,testimonials,programs and campus components

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Hero from "./components/Hero/Hero";
 import Programs from "./components/programs/Programs";
 import Title from "./components/Title/Title";
 import About from "./components/About/About";
+import Campus from "./components/Campus/Campus";
+import Testimonials from "./components/Testimonials/Testimonials";
 const App = () => {
   return(
    <div>
@@ -13,6 +15,10 @@ const App = () => {
       <Title subTitle='Our Program' title='What We Offer'/>
      <Programs/>
      <About/>
+     <Title subTitle='Gallery' title='Campus Photos'/>
+     <Campus/>
+     <Title subTitle='TESTIMONIALS' title='What a student says'/>
+     <Testimonials/>
     </div>
    
     
