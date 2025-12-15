a8f0bdc4c3d67e5e170ce0bbd55fa4945b7aafd9
Kabz Jose <josekabz00@gmail.com>
2025-12-15 21:13:14 +0300

    Added responsiveness on media query

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 import { Link } from 'react-scroll';
+import menu_icon from '../../assets/menu-icon.png'
 const Navbar = () => {
   const [sticky,setSticky]=useState(false)
   useEffect(()=>{
@@ -9,10 +10,14 @@ const Navbar = () => {
       window.scrollY > 600? setSticky(true): setSticky(false);
     })
   },[]);
+  const [mobileMenu,setMobileMenu]=useState(false);
+  const toggleMenu=()=>{
+   mobileMenu? setMobileMenu(false): setMobileMenu(true);
+  }
   return (
     <nav className={`container ${sticky? 'dark-nav':''}` }>
       <img src={logo} alt="" className='logo' />
-      <ul>
+      <ul className={mobileMenu? '':'hide-mobile-menu'}>
       <li>
           <Link to='hero' smooth={true} offset={0} duration={500}>
             Home
@@ -48,6 +53,7 @@ const Navbar = () => {
            </Link>
         </li>
         </ul>
+        <img src={menu_icon} className='menu-icon' alt=""onClick={toggleMenu} />
     </nav>
   )
 }
